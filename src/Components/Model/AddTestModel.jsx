6b58a0f8b7802f6daf770e43a489b000cd546bc4
RONIT PATEL <ronitpatel1810@gmail.com>
2024-05-31 12:15:47 +0530

    form handling

--- src/Components/Model/AddTestModel.jsx
+++ src/Components/Model/AddTestModel.jsx
@@ -1,7 +1,18 @@
 // import React from 'react'
 
+import { useState } from "react"
+
 // eslint-disable-next-line react/prop-types
 const AddTestModel = ({setTestMenu}) => {
+
+  const[testInput, setTestInput] = useState({"TestName":"", "TestDescription":"", "TestImageLink":"", "Price":"", "Fasting":"", "NormalRange":"", "AbnormalRange":"", "Date":""})
+
+  const handleTestInput = (e) => {
+    e.preventDefault()
+    setTestInput({...testInput,[e.target.name]:e.target.value})
+  }
+  console.log(testInput);
+
   return (
     <div className=" flex justify-center items-center w-full h-full fixed top-0 left-0 bg-[rgba(168,169,168,0.5)]">
         <div className=" bg-[#f9efe4] w-[340px] md:w-[700px] h-[580px] rounded-md">
@@ -19,42 +30,42 @@ const AddTestModel = ({setTestMenu}) => {
 
                 <div className=" md:p-3">
                   <label htmlFor="">Test Name</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="TestName" value={testInput.TestName} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className=" md:p-3">
                   <label htmlFor="">Test Description</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="TestDescription" value={testInput.TestDescription} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className=" md:p-3">
                   <label htmlFor="">Test Image Link</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="TestImageLink" value={testInput.TestImageLink} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className=" md:p-3">
                   <label htmlFor="">Price</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="Price" value={testInput.Price} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className=" md:p-3">
                   <label htmlFor="">Fasting</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="Fasting" value={testInput.Fasting} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className=" md:p-3">
                   <label htmlFor="">Normal Range</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="NormalRange" value={testInput.NormalRange} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className=" md:p-3">
-                  <label htmlFor="">Anormal Range</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <label htmlFor="">Abnormal Range</label><br />
+                  <input type="text" name="AbnormalRange" value={testInput.AbnormalRange} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className=" md:p-3">
                   <label htmlFor="">Date</label><br />
-                  <input type="date" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="date" name="Date" value={testInput.Date} onChange={(e)=>{handleTestInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <button className=" mt-3 md:mt-2 w-28 items-center border-[2px] border-[#102C57] rounded-lg px-5 py-2 hover:text-white hover:bg-[#102C57]" type="submit">Submit</button>
--- src/Components/Model/CreateNewModel.jsx
+++ src/Components/Model/CreateNewModel.jsx
@@ -1,7 +1,18 @@
 // import React from 'react'
 
+import { useState } from "react"
+
 // eslint-disable-next-line react/prop-types
 const Model = ( {setIsOpen, item} ) => {
+
+  const [input, setInput] = useState({"Name":"", "Mobile":"", "Adderss":"", "Age":"", "ExaminedBy":"", "ExaminedDate":"", "SelectTest":"", "ReportDate":""})
+
+  const handleInput = (e) => {
+    e.preventDefault()
+    setInput({...input,[e.target.name]:e.target.value})
+  }
+  console.log(input);
+
   return (
     <div className=" flex justify-center fixed top-0 left-0 h-screen w-screen bg-[rgba(154,158,160,0.5)]">
           <div className=" border-[2px] border-[#102C57] rounded-lg w-80 h-[600px] md:w-[800px] md:h-[500px] mt-12 md:mt-24 bg-[#f9efe4]">
@@ -14,46 +25,46 @@ const Model = ( {setIsOpen, item} ) => {
               <form className=" flex justify-center flex-wrap gap-1 md:gap-5">
               <div className="">
                   <label htmlFor="">Name</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="Name" value={input.Name} onChange={(e)=>{handleInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className="">
                   <label htmlFor="">Mobile No.</label><br />
-                  <input type="number" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="number" name="Mobile" value={input.Mobile} onChange={(e)=>{handleInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className="">
                   <label htmlFor="">Adderss</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="Adderss" value={input.Adderss} onChange={(e)=>{handleInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className="">
                   <label htmlFor="">Age</label><br />
-                  <input type="number" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="number" name="Age" value={input.Age} onChange={(e)=>{handleInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className="">
                   <label htmlFor="">Examined By</label><br />
-                  <input type="text" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="text" name="ExaminedBy" value={input.ExaminedBy} onChange={(e)=>{handleInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className="">
                   <label htmlFor="">Examined Date</label><br />
-                  <input type="date" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="date" name="ExaminedDate" value={input.ExaminedDate} onChange={(e)=>{handleInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div>
 
                 <div className="">
                   <label htmlFor="">Select Test</label><br />
-                  <select className="outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:h-9 w-72">
-                    <option>A</option>
-                    <option>B</option>
-                    <option>C</option>
+                  <select name="SelectTest" onChange={(e)=>{handleInput(e)}} className="outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:h-9 w-72">
+                    <option value={'A Test'}>A</option>
+                    <option value={'B Test'}>B</option>
+                    <option value={'C Test'}>C</option>
                   </select>
                 </div>
 
                 <div className="">
                   <label htmlFor="">Report Date</label><br />
-                  <input type="date" className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
+                  <input type="date" name="ReportDate" value={input.ReportDate} onChange={(e)=>{handleInput(e)}} className=" outline-none border-[2px] rounded-md border-[#102C57] px-4 h-7 md:py-4 w-72" />
                 </div> 
 
                 <button className=" m-5 w-28 items-center border-[2px] border-[#102C57] rounded-lg px-5 py-2 hover:text-white hover:bg-[#102C57]" type="submit">Submit</button>
